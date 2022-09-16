bc3e3cbf0b9a644f68529495a0c3e05608c9e191
afolabiopakunle <opakunleafolabi@gmail.com>
2022-09-16 08:10:49 +0100

    paused

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../services/message.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   ngOnInit() {
+  }
+
+  sendMessage(text) {
+    this.messageService.sendMessage(text)
   }
 
 }
--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class MessageService {
 
   constructor() { }
+
+  messagesSubject = new Subject<string>();
+
+  sendMessage(message: string) {
+    this.messagesSubject.next(message);
+    console.log(message)
+  }
+
+  receiveMessage() {
+    return this.messagesSubject.asObservable
+  }
 
 }
